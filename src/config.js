4af4e5c114d8372835296d885c6e9cb9553a1576
Xiaolei Wang <xwang@sandiego.edu>
2019-12-02 00:24:52 -0800

    successfully migrated initial tables for roomies and chores

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://choreshare_user:chore@localhost/choreshare',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://choreshare_user:chore@localhost/choreshare-test'
+    DB_URL: process.env.DATABASE_URL || 'postgresql://choreshare_user:chore@localhost/choreshare',
+    TEST_DB_URL: process.env.TEST_DATABASE_URL || 'postgresql://choreshare_user:chore@localhost/choreshare-test'
 }
--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = 'test'
+process.env.NODE_ENV = 'test';
 
 require('dotenv').config();
 const { expect } = require('chai');
