03a5ec1a7c5b2961a0f0dbb2a4664060962b591b
Xiaolei Wang <xwang@sandiego.edu>
2019-12-02 21:00:25 -0800

    added seed information for roomies and chores

---
+++ seeds/seed.choreshare_chores.sql
@@ -0,0 +1,8 @@
+INSERT INTO choreshare_chores(chore, roomie_id)
+VALUES
+    ('Take out the trash', 2),
+    ('Clean the litter box', 1),
+    ('Sweep the floor', 1),
+    ('Scrub the toilet', 2),
+    ('Dust the living room', 2),
+    ('Wash the dishes', 1);
---
+++ seeds/seed.choreshare_roomies.sql
@@ -0,0 +1,4 @@
+INSERT INTO choreshare_roomies (name, note)
+VALUES
+    ('Jane Rom', 'Rooming for 6 months, likes cats'),
+    ('James Mor', 'Rooming for 1 year, allergic to dogs');
