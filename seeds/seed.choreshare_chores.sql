84671cae44a8d129668dd1431f14c73d4377b177
Xiaolei Wang <xwang@sandiego.edu>
2019-12-05 22:52:37 -0800

    added checked value to seed data for chores

--- seeds/seed.choreshare_chores.sql
+++ seeds/seed.choreshare_chores.sql
@@ -1,8 +1,8 @@
-INSERT INTO choreshare_chores(chore, roomie_id)
+INSERT INTO choreshare_chores(chore, checked, roomie_id)
 VALUES
-    ('Take out the trash', 2),
-    ('Clean the litter box', 1),
-    ('Sweep the floor', 1),
-    ('Scrub the toilet', 2),
-    ('Dust the living room', 2),
-    ('Wash the dishes', 1);
+    ('Take out the trash', FALSE, 2),
+    ('Clean the litter box', FALSE, 1),
+    ('Sweep the floor', FALSE, 1),
+    ('Scrub the toilet', FALSE, 2),
+    ('Dust the living room', FALSE, 2),
+    ('Wash the dishes', FALSE, 1);
