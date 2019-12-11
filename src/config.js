e7bc8f0849c997cf349657a91d635b4d49825b28
Xiaolei Wang <xwang@sandiego.edu>
2019-12-10 20:37:57 -0800

    updated files for deployment to Heroku

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://choreshare_user:chore@localhost/choreshare',
-    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://choreshare_user:chore@localhost/choreshare-test'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://choreshare_user:chore@localhost/choreshare',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://choreshare_user:chore@localhost/choreshare-test'
 }
